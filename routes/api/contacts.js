f187aade124f20ca16e6f4453083fb355c85996f
SerhiiIsaev <97984230+SerhiiIsaev@users.noreply.github.com>
2022-12-13 22:41:03 +0200

    add folder-controllers

--- routes/api/contacts.js
+++ routes/api/contacts.js
@@ -1,90 +1,27 @@
 const express = require('express')
-const Contact = require('../../models/contacts')
-const {HttpError} = require('../../helpers')
-
 const { validateBody } = require('../../middlewares')
+const {
+  addOne,
+  deleteBuId,
+  getAll,
+  getById,
+  updateById,
+  updateStatus
+} = require("../../controllers/contacts");
 const {validatingSchema, updateFavoriteSchema} = require('../../Schemas/validatingShema')
 const router = express.Router()
 
+router.get('/', getAll)
 
+router.get('/:id', getById)
 
-router.get('/', async (req, res, next) => {
-  try {
-    const result = await Contact.find()
-    res.json(result)
-  } catch (error) {
-    next(error)
-  }
-  
-})
+router.post('/',validateBody(validatingSchema), addOne)
 
-router.get('/:id', async (req, res, next) => {
-  try {
-    console.log(req.params.id)
-    const result = await Contact.findById(req.params.id)
-    if (!result) {
-      throw HttpError(404, 'Not Found')
-    }
-    res.json(result)
-  } catch (error) {
-    next(error)
-  }
-  
-})
+router.delete('/:id', deleteBuId)
 
-router.post('/',validateBody(validatingSchema), async (req, res, next) => {
-  try {
-    const result = await Contact.create(req.body)
-    res.status(201).json(result)
-  } catch (error) {
-    next(error)
-  }
-})
+router.put('/:id',validateBody(validatingSchema), updateById)
 
-router.delete('/:id', async (req, res, next) => {
-  try {
-    const result = await Contact.findByIdAndRemove(req.params.id)
-    if (!result) {
-      throw HttpError(404, "Not found")
-    }
-    res.json({
-      message: "Contact deleted"
-    })
-  } catch (error) {
-    next(error)
-  }
-})
+router.patch('/:id/favorite',validateBody(updateFavoriteSchema), updateStatus)
 
-router.put('/:id',validateBody(validatingSchema), async (req, res, next) => {
-  try {
-    const result = await Contact.findByIdAndUpdate(
-      req.params.id,
-      req.body,
-      { new: true }
-    )
-    if (!result) {
-      throw HttpError(404, "Not found")
-    }
-    res.json(result)
-  } catch (error) {
-    next(error)
-  }
-})
-
-router.patch('/:id/favorite',validateBody(updateFavoriteSchema), async (req, res, next) => {
-  try {
-    const result = await Contact.findByIdAndUpdate(
-      req.params.id,
-      req.body,
-      { new: true }
-    )
-    if (!result) {
-      throw HttpError(404, "Not found")
-    }
-    res.json(result)
-  } catch (error) {
-    next(error)
-  }
-})
 
 module.exports = router
