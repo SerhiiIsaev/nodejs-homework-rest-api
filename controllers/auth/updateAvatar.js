7bb0ef33fa9972173e06294845c6890392c2dac7
SerhiiIsaev <97984230+SerhiiIsaev@users.noreply.github.com>
2023-01-12 21:20:28 +0200

    add updateAvatar

--- controllers/auth/updateAvatar.js
+++ controllers/auth/updateAvatar.js
@@ -1,22 +1,34 @@
 const fs = require("fs/promises")
 const path = require("path")
+const jimp = require("jimp");
 
-const {User} = require("../../models/user")
+const User = require("../../models/user")
 
 const avatarsDir = path.join(__dirname, "../../", "public", "avatars");
 
-const updateAvatar = async(req, res)=> {
-    const {path: tempUpload, originalname} = req.file;
-    const {_id} = req.user;
-    const filename = `${_id}_${originalname}`;
-    const resultUpload = path.join(avatarsDir, filename);
-    await fs.rename(tempUpload, resultUpload);
-    const avatarURL = path.join("avatars", filename);
-    await User.findByIdAndUpdate(_id, {avatarURL});
+const updateAvatar = async (req, res, next) => {
+    try {
+        const {path: tempUpload, originalname} = req.file;
+        const { _id } = req.user;
+        
+        const image = await jimp.read(tempUpload);
+        await image.resize(250, 250);
+        await image.writeAsync(tempUpload);
 
-    res.json({
-        avatarURL,
-    })
+        const filename = `${_id}_${originalname}`;
+        const resultUpload = path.join(avatarsDir, filename);
+        await fs.rename(tempUpload, resultUpload);
+        const avatarURL = path.join("avatars", filename);
+        console.log(avatarURL)
+        await User.findByIdAndUpdate(_id, {avatarURL});
+
+        res.json({
+            avatarURL,
+        })
+    } catch (error) {
+        next(error)
+    }
+    
 }
 
 module.exports = updateAvatar;
--- middlewares/index.js
+++ middlewares/index.js
@@ -1,7 +1,7 @@
 const validateBody = require("./validatingBody")
 const validateFavorite = require("./validatingFavorite")
-const authenticate = require('./authenticate')
-const upload = require('./upload')
+const authenticate = require("./authenticate")
+const upload = require("./upload")
 module.exports = {
     validateBody,
     validateFavorite,
